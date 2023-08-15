fe004a7ddc2fca85e82dc9278457775032883b4c
Emanuromet <emanuromet@gmail.com>
2023-08-15 14:23:48 -0400

    add code

--- js/app.js
+++ js/app.js
@@ -1,10 +1,4 @@
 let formUsername = document.getElementById('formUsername');
 let formPassword = document.getElementById('formPassword');
-let formBtn = document.getElementById('formBtn');
+let formRegisterUser = document.getElementById('formRegisterUser');
 
-formBtn.addEventListener('click', (e) =>{
-    e.preventDefault();
-    let username = formUsername.value;
-    let passwrod = formPassword.value;
-    console.log(username,passwrod);
-})
