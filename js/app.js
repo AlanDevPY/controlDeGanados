c744ba231deaebf1228fefa7d5e8e9bc24730f6c
Emanuromet <emanuromet@gmail.com>
2023-08-15 14:54:14 -0400

    add code

--- js/app.js
+++ js/app.js
@@ -2,3 +2,4 @@ let formUsername = document.getElementById('formUsername');
 let formPassword = document.getElementById('formPassword');
 let formRegisterUser = document.getElementById('formRegisterUser');
 
+
